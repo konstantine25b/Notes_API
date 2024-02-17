42c18908e68754026513d4a0bf8ffc1a6a30911a
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-17 17:54:27 +0100

    added env adn changed docker docs

--- dockerproject/dockerproject/settings.py
+++ dockerproject/dockerproject/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cvee$id%hz%+(0twb*b@2vqz@xr)^@_j=4f+s8_3t$a2f&zvlm'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

5f9549a280dad210577e186e6c3dc832e99b00ec
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-17 18:03:09 +0100

    made almost full env

--- dockerproject/dockerproject/settings.py
+++ dockerproject/dockerproject/settings.py
@@ -65,12 +65,24 @@ WSGI_APPLICATION = 'dockerproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'POST': os.getenv('DB_POST'),
     }
 }
+
 
 
 # Password validation
